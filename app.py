12d44bb566034c8f56b9bcb78e60b54cf981359c
Phillip-Healy <phillip.healy9@mail.dcu.ie>
2021-07-01 11:42:17 +0000

    Add news and games to index populated from DB

--- app.py
+++ app.py
@@ -19,7 +19,9 @@ mongo = PyMongo(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    news = list(mongo.db.news.find())
+    games = list(mongo.db.games.find())
+    return render_template("index.html", news=news, games=games)
 
 
 if __name__ == "__main__":
